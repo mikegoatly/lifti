9998d74f86d7a442f484db3f89ef347f11faee40
Mike Goatly <mikegoatly@hotmail.com>
2019-09-11 14:24:26 +0100

    Added tests for StringBuilder.SequenceEqual extension method

---
+++ Lifti.Tests/Preprocessing/StringBuilderExtensionTests.SequenceEqual.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Lifti.Tests.Preprocessing
+{
+    public class StringBuilderExtensionTests_SequenceEqual
+    {
+        [Theory]
+        [InlineData("", "1")]
+        [InlineData("1", "")]
+        [InlineData("11", "1")]
+        public void WhenStringsDifferentLengthsShouldReturnFalse(string a, string b)
+        {
+            new StringBuilder(a).SequenceEqual(b.ToCharArray()).Should().BeFalse();
+        }
+
+        [Fact]
+        public void WhenBothStringsEmptyShouldReturnTrue()
+        {
+            new StringBuilder().SequenceEqual(Array.Empty<char>()).Should().BeTrue();
+        }
+
+        [Fact]
+        public void WhenBothStringsHaveSameCharactersShouldReturnTrue()
+        {
+            new StringBuilder("test").SequenceEqual("test".ToCharArray()).Should().BeTrue();
+        }
+
+        [Fact]
+        public void WhenBothStringsHaveDifferentCharactersShouldReturnFalse()
+        {
+            new StringBuilder("best").SequenceEqual("test".ToCharArray()).Should().BeFalse();
+        }
+    }
+}
