c7b2aa1815bd9e2eca701d988d9046e8e0b7c07d
Mike Goatly <mikegoatly@hotmail.com>
2019-09-13 14:40:23 +0100

    Reduced GC pressure by using List<> vs ImmutableList<> when parsing

--- Lifti.Core/Preprocessing/Token.cs
+++ Lifti.Core/Preprocessing/Token.cs
@@ -1,34 +1,35 @@
 ﻿using System.Collections.Generic;
-using System.Collections.Immutable;
 
 namespace Lifti
 {
     public class Token
     {
+        private readonly List<Range> locations;
+
         public Token(string token, Range location)
         {
-            this.Locations = ImmutableList<Range>.Empty.Add(location);
+            this.locations = new List<Range> { location };
             this.Value = token;
         }
 
         public Token(string token, params Range[] locations)
         {
-            this.Locations = locations.ToImmutableList();
+            this.locations = new List<Range>(locations);
             this.Value = token;
         }
 
         public Token(string token, IReadOnlyList<Range> locations)
         {
-            this.Locations = locations.ToImmutableList();
+            this.locations = new List<Range>(locations);
             this.Value = token;
         }
 
-        public ImmutableList<Range> Locations { get; set; }
+        public IReadOnlyList<Range> Locations => this.locations;
         public string Value { get; }
 
         public void AddLocation(Range location)
         {
-            this.Locations = this.Locations.Add(location);
+            this.locations.Add(location);
         }
     }
 }
