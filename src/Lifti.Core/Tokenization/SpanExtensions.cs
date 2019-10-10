c8ef745b5b098fa1db2a1756ff618260d23ff8c1
Mike Goatly <mikegoatly@hotmail.com>
2019-10-10 20:25:45 +0100

    Optimisations

--- src/Lifti.Core/Tokenization/SpanExtensions.cs
+++ src/Lifti.Core/Tokenization/SpanExtensions.cs
@@ -2,15 +2,10 @@
 
 namespace Lifti.Tokenization
 {
-    public static class SpanExtensions
+    internal static class SpanExtensions
     {
         public static bool SequenceEqual(this ReadOnlySpan<char> word, string chars)
         {
-            if (chars is null)
-            {
-                throw new ArgumentNullException(nameof(chars));
-            }
-
             if (chars.Length != word.Length)
             {
                 return false;
