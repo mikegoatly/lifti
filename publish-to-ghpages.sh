a25600ce155ced7b120e44b402c8e13601cf5b5a
Mike Goatly <mikegoatly@hotmail.com>
2020-07-14 22:36:24 +0100

    Updated publish script

--- publish-to-ghpages.sh
+++ publish-to-ghpages.sh
@@ -8,24 +8,25 @@ then
 fi
 
 echo "Deleting old publication"
-rm -rf public
-mkdir public
+rm -rf docs/public
+mkdir docs/public
 git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
+cd docs
+git worktree add -B gh-pages public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
 
 echo "Generating site"
-cd docs
 hugo.exe
-cd ..
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+
+cd ../..
 
 # echo "Pushing to github"
 # git push --all
