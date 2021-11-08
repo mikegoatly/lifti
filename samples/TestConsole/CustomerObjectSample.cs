5389c9ae1a9334b552a793f80db245c6b2a36409
Mike Goatly <4577868+mikegoatly@users.noreply.github.com>
2021-11-08 17:04:59 +0000

    Added sharding example

---
+++ samples/TestConsole/CustomerObjectSample.cs
@@ -0,0 +1,27 @@
+﻿using Lifti;
+using Lifti.Tokenization.TextExtraction;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    public static class CustomerObjectSample
+    {
+        public class Customer
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string ProfileHtml { get; set; }
+        }
+
+        public static async Task RunAsync()
+        {
+            var index = new FullTextIndexBuilder<int>()
+                .WithObjectTokenization<Customer>(o => o
+                    .WithKey(c => c.Id)
+                    .WithField("Name", c => c.Name)
+                    .WithField("Profile", c => c.ProfileHtml, textExtractor: new XmlTextExtractor())
+                )
+                .Build();
+        }
+    }
+}
---
+++ samples/TestConsole/ShardedIndexExample.cs
@@ -0,0 +1,65 @@
+﻿using Lifti;
+using Lifti.Serialization.Binary;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    public static class ShardedIndexExample
+    {
+        public class ShardedIndex
+        {
+            private static readonly BinarySerializer<int> serializer = new BinarySerializer<int>();
+            private readonly Dictionary<string, FullTextIndex<int>> indexShards = new Dictionary<string, FullTextIndex<int>>();
+            private readonly SemaphoreSlim syncObject = new SemaphoreSlim(1);
+
+            public async Task<FullTextIndex<int>> GetIndexAsync(string partitionKey, CancellationToken cancellationToken = default)
+            {
+                if (!await syncObject.WaitAsync(TimeSpan.FromSeconds(1), cancellationToken))
+                {
+                    throw new Exception("Timeout waiting for lock");
+                }
+
+                try
+                {
+                    if (!indexShards.TryGetValue(partitionKey, out var index))
+                    {
+                        // Create the index for this shard
+                        index = new FullTextIndexBuilder<int>()
+                                // e.g. shard peristance using the partition key as the file name
+                                .WithIndexModificationAction(async (idx) =>
+                                {
+                                    using (var fileStream = File.OpenWrite($"{partitionKey}.dat"))
+                                    {
+                                        await serializer.SerializeAsync(idx, fileStream);
+                                    }
+                                })
+                                .Build();
+
+                        // Deserialize initial state, if appropriate
+                        var serializedFile = new FileInfo($"{partitionKey}.dat");
+                        if (serializedFile.Exists)
+                        {
+                            using (var stream = serializedFile.OpenRead())
+                            {
+                                await serializer.DeserializeAsync(index, stream);
+                            }
+                        }
+
+                        // Store the index for the next access
+                        indexShards[partitionKey] = index;
+                    }
+
+                    return index;
+                }
+                finally
+                {
+                    syncObject.Release();
+                }
+            }
+        }
+    }
+}
