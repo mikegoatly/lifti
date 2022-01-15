8583bd2be715c8a647850cc2ba515d26e202a0df
Mike Goatly <4577868+mikegoatly@users.noreply.github.com>
2022-01-15 16:05:27 +0000

    Added initial fuzzy match implementation

---
+++ src/Lifti.Core/Querying/QueryParts/DoubleBufferedList.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Lifti.Querying.QueryParts
+{
+    internal class DoubleBufferedList<T> : IEnumerable<T>
+    {
+        private List<T> current = new List<T>();
+        private List<T> swap = new List<T>();
+
+        public DoubleBufferedList()
+        {
+        }
+
+        public DoubleBufferedList(params T[] initialData)
+        {
+            this.current.AddRange(initialData);
+        }
+
+        public void Add(T item)
+        {
+            this.swap.Add(item);
+        }
+
+        public int Count => this.current.Count;
+
+        public void Swap()
+        {
+            var tempStack = this.current;
+            tempStack.Clear();
+            this.current = this.swap;
+            this.swap = tempStack;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this.current.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ src/Lifti.Core/Querying/QueryParts/FuzzyWordQueryPart.cs
@@ -0,0 +1,223 @@
+﻿#define TRACK_MATCH_STATE_TEXT
+// Enabling TRACK_MATCH_STATE_TEXT in DEBUG builds will allow you inspect the state of a fuzzy match
+// as it is being processed using the MatchText property. This is not available in release builds for performance.
+
+using System;
+
+namespace Lifti.Querying.QueryParts
+{
+    /// <summary>
+    /// An <see cref="IQueryPart"/> that matches items that contain an fuzzy match for the given text.
+    /// </summary>
+    public class FuzzyWordQueryPart : WordQueryPart
+    {
+        private readonly int maxEditDistance;
+
+        private enum EditKind
+        {
+            Substitution,
+            Deletion
+        }
+
+        private struct FuzzyMatchState
+        {
+#if DEBUG && TRACK_MATCH_STATE_TEXT
+            public FuzzyMatchState(
+                IIndexNavigatorBookmark bookmark,
+                int editCount,
+                int wordIndex,
+                string matchText) : this()
+            {
+                this.Bookmark = bookmark;
+                this.EditDistance = editCount;
+                this.WordIndex = wordIndex;
+                this.MatchText = matchText;
+            }
+#else
+            public FuzzyMatchState(IIndexNavigatorBookmark bookmark, int editDistance, int wordIndex) : this()
+            {
+                this.Bookmark = bookmark;
+                this.EditDistance = editDistance;
+                this.WordIndex = wordIndex;
+            }
+#endif
+
+            public FuzzyMatchState(IIndexNavigatorBookmark bookmark)
+                : this(
+                      bookmark,
+                      0,
+                      0
+#if DEBUG && TRACK_MATCH_STATE_TEXT
+                      , string.Empty
+#endif
+                      )
+            {
+            }
+
+            public IIndexNavigatorBookmark Bookmark { get; }
+
+            public int EditDistance { get; }
+
+            /// <summary>
+            /// The index that this state is currently matching in the target word.
+            /// </summary>
+            public int WordIndex { get; }
+
+#if DEBUG && TRACK_MATCH_STATE_TEXT
+            public string MatchText { get; }
+#endif
+        }
+
+        /// <summary>
+        /// Constructs a new instance of <see cref="ExactWordQueryPart"/>.
+        /// </summary>
+        public FuzzyWordQueryPart(string word, int maxEditDistance)
+            : base(word)
+        {
+            this.maxEditDistance = maxEditDistance;
+        }
+
+        /// <inheritdoc/>
+        public override IntermediateQueryResult Evaluate(Func<IIndexNavigator> navigatorCreator, IQueryContext queryContext)
+        {
+            if (navigatorCreator == null)
+            {
+                throw new ArgumentNullException(nameof(navigatorCreator));
+            }
+
+            using var navigator = navigatorCreator();
+            var results = IntermediateQueryResult.Empty;
+            var stateStore = new DoubleBufferedList<FuzzyMatchState>(new FuzzyMatchState(navigator.CreateBookmark()));
+
+            var characterCount = 0;
+            do
+            {
+                foreach (var state in stateStore)
+                {
+                    var wordIndex = state.WordIndex;
+                    var bookmark = state.Bookmark;
+                    bookmark.Apply();
+
+                    if (state.WordIndex == this.Word.Length)
+                    {
+                        // Don't allow matches that have consisted entirely of edits
+                        if (characterCount > state.EditDistance)
+                        {
+                            // We're out of search characters for this state.
+                            if (navigator.HasExactMatches)
+                            {
+                                results = results.Union(navigator.GetExactMatches());
+                            }
+                            else
+                            {
+                                // Assume there could be missing characters at the end
+                                AddBookmarksForAllTraversableCharacters(navigator, stateStore, state, EditKind.Deletion);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        var currentCharacter = this.Word[wordIndex];
+                        if (navigator.Process(currentCharacter))
+                        {
+                            // The character matched successfully, so potentially no edits incurred, just move to the next character
+                            stateStore.Add(
+                                new FuzzyMatchState(
+                                    navigator.CreateBookmark(),
+                                    state.EditDistance,
+                                    wordIndex + 1
+#if DEBUG && TRACK_MATCH_STATE_TEXT
+                                , state.MatchText + currentCharacter
+#endif
+                                ));
+                        }
+                        else
+                        {
+                            // First skip this character (assume extra character inserted), but don't move the navigator on
+                            if (state.EditDistance < this.maxEditDistance)
+                            {
+                                stateStore.Add(
+                                    new FuzzyMatchState(
+                                        bookmark,
+                                        state.EditDistance + 1,
+                                        wordIndex + 1
+#if DEBUG && TRACK_MATCH_STATE_TEXT
+                                        , state.MatchText + $"(+{currentCharacter})"
+#endif
+                            ));
+                            }
+
+                            // Also try skipping this character (assume omission) by just moving on in the navigator
+                            AddBookmarksForAllTraversableCharacters(navigator, stateStore, state, EditKind.Deletion);
+                        }
+
+                        // Always assume this could be a substituted character
+                        AddBookmarksForAllTraversableCharacters(navigator, stateStore, state, EditKind.Substitution, except: currentCharacter);
+                    }
+                }
+
+                stateStore.Swap();
+
+                characterCount++;
+            }
+            while (stateStore.Count > 0);
+
+            // TODO adjust score based on edit count
+            return results;
+        }
+
+        private void AddBookmarksForAllTraversableCharacters(
+            IIndexNavigator navigator,
+            DoubleBufferedList<FuzzyMatchState> stateStore,
+            FuzzyMatchState currentState,
+            EditKind editKind,
+            char? except = null)
+        {
+            var bookmark = currentState.Bookmark;
+            var nextEditDistance = currentState.EditDistance + 1;
+            var nextIndex = editKind switch
+            {
+                EditKind.Deletion => currentState.WordIndex,
+                _ => currentState.WordIndex + 1
+            };
+
+            if (nextEditDistance > this.maxEditDistance)
+            {
+                // Stop processing - we've reached the maximum number of allowed edits
+                return;
+            }
+
+            bookmark.Apply();
+            foreach (char c in navigator.EnumerateNextCharacters())
+            {
+                if (except == c)
+                {
+                    continue;
+                }
+
+                bookmark.Apply();
+                navigator.Process(c);
+                stateStore.Add(
+                    new FuzzyMatchState(
+                        navigator.CreateBookmark(),
+                        nextEditDistance,
+                        nextIndex
+#if DEBUG && TRACK_MATCH_STATE_TEXT
+                        , editKind switch
+                        {
+                            EditKind.Substitution => currentState.MatchText + $"(-{this.Word[currentState.WordIndex]}{c}?)",
+                            EditKind.Deletion => currentState.MatchText + $"(-{c})",
+                            _ => throw new InvalidOperationException("Unsupported edit kind!")
+                        }
+#endif
+                        ));
+            }
+        }
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return this.Word;
+        }
+    }
+}
