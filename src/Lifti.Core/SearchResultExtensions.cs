1d9ab661ceeb86a98e2b00a602fcd11ef97bf7a4
Mike Goatly <4577868+mikegoatly@users.noreply.github.com>
2022-02-05 17:44:39 +0000

    Added OrderByField to re-order results by scores of a single field

---
+++ src/Lifti.Core/SearchResultExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Lifti
+{
+    /// <summary>
+    /// Extension methods for LIFTI search results.
+    /// </summary>
+    public static class SearchResultExtensions
+    {
+        /// <summary>
+        /// Re-orders the search results, only considering scores for a single field.
+        /// </summary>
+        /// <param name="searchResults">The search results to re-order.</param>
+        /// <param name="fieldName">The name of the field to order the search results by.</param>
+        public static IEnumerable<SearchResult<TKey>> OrderByField<TKey>(this IEnumerable<SearchResult<TKey>> searchResults, string fieldName)
+        {
+            return searchResults.OrderByDescending(
+                r => r.FieldMatches.Sum(f => f.FoundIn == fieldName ? f.Score : 0D));
+        }
+    }
+}
