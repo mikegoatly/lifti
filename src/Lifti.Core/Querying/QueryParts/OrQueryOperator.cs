e452b526ad6a61940002a4537c8a8462b77de6e8
Mike Goatly <4577868+mikegoatly@users.noreply.github.com>
2022-12-08 22:14:27 +0000

    Check for null arguments

--- src/Lifti.Core/Querying/QueryParts/OrQueryOperator.cs
+++ src/Lifti.Core/Querying/QueryParts/OrQueryOperator.cs
@@ -40,6 +40,11 @@ namespace Lifti.Querying.QueryParts
         /// <exception cref="QueryParserException">Thrown when <paramref name="queryParts"/> is empty.</exception>
         public static IQueryPart CombineAll(IEnumerable<IQueryPart> queryParts)
         {
+            if (queryParts is null)
+            {
+                throw new ArgumentNullException(nameof(queryParts));
+            }
+
             IQueryPart? current = null;
             foreach (var queryPart in queryParts)
             {
