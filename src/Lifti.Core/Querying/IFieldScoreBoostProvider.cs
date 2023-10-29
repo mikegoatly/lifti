8fee3d1f8b1c0f1914b15c2ef1befe211af56963
Mike Goatly <mikegoatly@hotmail.com>
2023-10-29 08:53:17 +0000

    Updated code comment again

--- src/Lifti.Core/Querying/IFieldScoreBoostProvider.cs
+++ src/Lifti.Core/Querying/IFieldScoreBoostProvider.cs
@@ -1,7 +1,7 @@
 ﻿namespace Lifti.Querying
 {
     /// <summary>
-    /// A provider capable of providing the score boost for a given field.
+    /// A class capable of providing the score boost for a given field.
     /// </summary>
     internal interface IFieldScoreBoostProvider
     {
