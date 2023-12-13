47f9452f65dbfaa109c47abc03f332667f3d66b8
Mike Goatly <mikegoatly@hotmail.com>
2023-12-13 21:38:33 +0000

    Add .NET 8 as a target

---
+++ test/PerformanceProfiling/StartsWithMultiCharacterWildcardBenchmark.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using Lifti;
+using System.Threading.Tasks;
+
+namespace PerformanceProfiling
+{
+    [ShortRunJob(RuntimeMoniker.Net481)]
+    [ShortRunJob(RuntimeMoniker.Net80)]
+    [ShortRunJob(RuntimeMoniker.Net70)]
+    [ShortRunJob(RuntimeMoniker.Net60)]
+    [RankColumn, MemoryDiagnoser]
+    public class StartsWithMultiCharacterWildcardBenchmark : IndexBenchmarkBase
+    {
+        private IFullTextIndex<int> index;
+
+        [GlobalSetup]
+        public async Task SetUp()
+        {
+            this.index = CreateNewIndex(4);
+            await this.PopulateIndexAsync(this.index);
+        }
+
+        [Benchmark]
+        public object Searching()
+        {
+            return this.index.Search("*ion");
+        }
+    }
+}
