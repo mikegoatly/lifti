333d79d79134de2dfa80ccfbb52c110afc51b5ab
Mike Goatly <4577868+mikegoatly@users.noreply.github.com>
2023-12-29 09:31:22 +0000

    Improve DocumentTokenMatchMap mutation perf

--- src/Lifti.Core/DocumentTokenMatchMap.cs
+++ src/Lifti.Core/DocumentTokenMatchMap.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+
+#if NETSTANDARD
 using System.Linq;
+#endif
 
 namespace Lifti
 {
@@ -24,30 +27,28 @@ namespace Lifti
             }
 
             // Copy the original matches except any that have been expressly removed
-            var remainingOriginalMatches = this.removed == null
-                ? this.original.Enumerate()
-                : this.original.Enumerate().Where(x => !this.removed.Contains(x.documentId));
-
-            Dictionary<int, IReadOnlyList<IndexedToken>> mutatedMatches;
-            if (this.mutated == null)
+            Dictionary<int, IReadOnlyList<IndexedToken>> mutatedMatches = new(this.original.DocumentTokenLookup);
+            if (this.removed != null)
             {
-                // Just create a new dictionary with the remaining original matches
-                mutatedMatches = remainingOriginalMatches.ToDictionary(x => x.documentId, x => x.indexedTokens);
-            }
-            else
-            {
-                // Copy any unmutated matches that haven't been removed into the mutated matches dictionary
-                mutatedMatches = this.mutated.ToDictionary(x => x.Key, x => (IReadOnlyList<IndexedToken>)x.Value);
-                foreach (var (documentId, indexedTokens) in remainingOriginalMatches)
+                foreach (var documentId in this.removed)
                 {
-#if NETSTANDARD
-                    if (!mutatedMatches.ContainsKey(documentId))
-                    {
-                        mutatedMatches.Add(documentId, indexedTokens);
-                    }
-#else
-                    mutatedMatches.TryAdd(documentId, indexedTokens);
+                    mutatedMatches.Remove(documentId);
+                }
+            }
+
+            if (this.mutated != null)
+            {
+#if !NETSTANDARD
+                // Avoid re-allocations by ensuring the dictionary has enough capacity to hold all the new items
+                // In some situations this may actually be more than we need (A large number of documents have
+                // been reindexed), but this is better in most cases. We could track the number of "new" documents
+                // and only increase the capacity by that amount...
+                mutatedMatches.EnsureCapacity(mutatedMatches.Count + this.mutated.Count);
 #endif
+
+                foreach (var documentMutation in this.mutated)
+                {
+                    mutatedMatches[documentMutation.Key] = documentMutation.Value;
                 }
             }
 
@@ -97,14 +98,12 @@ namespace Lifti
     /// </summary>
     public readonly struct DocumentTokenMatchMap : IEquatable<DocumentTokenMatchMap>
     {
-        private readonly Dictionary<int, IReadOnlyList<IndexedToken>> documentTokens;
-
         internal DocumentTokenMatchMap(IEnumerable<KeyValuePair<int, IReadOnlyList<IndexedToken>>> data)
         {
 #if NETSTANDARD
-            this.documentTokens = data.ToDictionary(x => x.Key, x => x.Value);
+            this.DocumentTokenLookup = data.ToDictionary(x => x.Key, x => x.Value);
 #else
-            this.documentTokens = new(data);
+            this.DocumentTokenLookup = new(data);
 #endif
         }
 
@@ -116,7 +115,7 @@ namespace Lifti
         /// </param>
         public DocumentTokenMatchMap(Dictionary<int, IReadOnlyList<IndexedToken>> data)
         {
-            this.documentTokens = data;
+            this.DocumentTokenLookup = data;
         }
 
         /// <summary>
@@ -124,17 +123,19 @@ namespace Lifti
         /// </summary>
         public static DocumentTokenMatchMap Empty { get; } = new DocumentTokenMatchMap(Array.Empty<KeyValuePair<int, IReadOnlyList<IndexedToken>>>());
 
+        internal Dictionary<int, IReadOnlyList<IndexedToken>> DocumentTokenLookup { get; }
+
         /// <summary>
         /// Gets the number of documents in the map.
         /// </summary>
-        public int Count => this.documentTokens.Count;
+        public int Count => this.DocumentTokenLookup.Count;
 
         /// <summary>
         /// Enumerates all the document matches in the map.
         /// </summary>
         public IEnumerable<(int documentId, IReadOnlyList<IndexedToken> indexedTokens)> Enumerate()
         {
-            foreach (var document in this.documentTokens)
+            foreach (var document in this.DocumentTokenLookup)
             {
                 yield return (document.Key, document.Value);
             }
@@ -145,7 +146,7 @@ namespace Lifti
         /// </summary>
         public bool TryGetValue(int documentId, [NotNullWhen(true)] out IReadOnlyList<IndexedToken>? tokens)
         {
-            return this.documentTokens.TryGetValue(documentId, out tokens);
+            return this.DocumentTokenLookup.TryGetValue(documentId, out tokens);
         }
 
         /// <summary>
@@ -157,7 +158,7 @@ namespace Lifti
         /// <returns></returns>
         internal List<IndexedToken> StartMutation(int documentId)
         {
-            if (this.documentTokens.TryGetValue(documentId, out var indexedTokens))
+            if (this.DocumentTokenLookup.TryGetValue(documentId, out var indexedTokens))
             {
                 return new List<IndexedToken>(indexedTokens);
             }
@@ -172,7 +173,7 @@ namespace Lifti
         /// </summary>
         public bool HasDocument(int documentId)
         {
-            return this.documentTokens.ContainsKey(documentId);
+            return this.DocumentTokenLookup.ContainsKey(documentId);
         }
 
         /// <inheritdoc/>
@@ -186,13 +187,13 @@ namespace Lifti
         public bool Equals(DocumentTokenMatchMap other)
         {
             // Because we're immutable, we can just compare the references
-            return this.documentTokens == other.documentTokens;
+            return this.DocumentTokenLookup == other.DocumentTokenLookup;
         }
 
         /// <inheritdoc/>
         public override int GetHashCode()
         {
-            return this.documentTokens.GetHashCode();
+            return this.DocumentTokenLookup.GetHashCode();
         }
 
         /// <inheritdoc/>
