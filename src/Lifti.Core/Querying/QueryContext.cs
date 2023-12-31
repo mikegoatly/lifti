111e0944b2d52420b19f3dd2f3112f6cf8215f49
Mike Goatly <4577868+mikegoatly@users.noreply.github.com>
2023-12-31 14:37:21 +0000

    Seal it

--- src/Lifti.Core/Querying/QueryContext.cs
+++ src/Lifti.Core/Querying/QueryContext.cs
@@ -6,7 +6,7 @@ namespace Lifti.Querying
     /// <summary>
     /// Manages context during the execution of a query, allowing for aspects like field filters to be applied.
     /// </summary>
-    public record QueryContext(byte? FilterToFieldId = null)
+    public sealed record QueryContext(byte? FilterToFieldId = null)
     {
         /// <summary>
         /// Gets an empty query context.
